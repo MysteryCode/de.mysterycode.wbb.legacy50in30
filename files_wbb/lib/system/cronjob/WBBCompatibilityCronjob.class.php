d996b7733cc363742112d02c5e7531dacc88e9a5
mutec <webmaster@mysterycode.de>
2018-01-16 23:14:41 +0100

    cronjob removing wsc 3.1-excludes for wbb 5.0 updates

---
+++ files_wbb/lib/system/cronjob/WBBCompatibilityCronjob.class.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace wcf\system\cronjob;
+
+use wcf\data\cronjob\Cronjob;
+use wcf\system\WCF;
+
+class WBBCompatibilityCronjob extends AbstractCronjob {
+	/**
+	 * @inheritDoc
+	 */
+	public function execute(Cronjob $cronjob) {
+		parent::execute($cronjob);
+		
+		$sql = "DELETE FROM     wcf".WCF_N."_package_update_exclusion
+			WHERE           excludedPackageVersion = ?
+					AND excludedPackage = ?
+					AND packageUpdateVersionID IN (
+						SELECT  packageUpdateVersionID
+						FROM    wcf".WCF_N."_package_update_version
+						WHERE   packageVersib LIKE ?
+							AND packageUpdateID = (
+							SELECT  packageUpdateID
+							FROM    wcf".WCF_N."_package_update
+							WHERE   package = ?
+						)
+					)";
+		$statement = WCF::getDB()->prepareStatement($sql);
+		$statement->execute(['3.1.0 Alpha 1', 'com.woltlab.wcf', '5.0.%', 'com.woltlab.wbb']);
+		
+		$sql = "DELETE FROM     wcf".WCF_N."_package_exclusion
+			WHERE           excludedPackageVersion = ?
+					AND excludedPackage = ?
+					AND packageID = (
+						SELECT  packageID
+						FROM    wcf".WCF_N."_package
+						WHERE   packageVersion LIKE ?
+							AND package = ?
+					)";
+		$statement = WCF::getDB()->prepareStatement($sql);
+		$statement->execute(['3.1.0 Alpha 1', 'com.woltlab.wcf', '5.0.%', 'com.woltlab.wbb']);
+	}
+}
